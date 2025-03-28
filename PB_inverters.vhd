f65cff5d40eaf95a8f48a66c1f527eeb7824bc29
Naman Biyani <biyaninaman1@gmail.com>
2025-03-27 23:06:12 -0400

    pb_inverters commented

--- PB_inverters.vhd
+++ PB_inverters.vhd
@@ -1,19 +1,26 @@
+-- Members: Naman Biyani and Tanmay Shah
+-- LS206_T20_LAB4
+
+-- imports
 library ieee;
 use ieee.std_logic_1164.all;
 
-
+-- Define Entity for PB_Inverters
 entity PB_inverters is port (
-	rst_n				: in	std_logic;
-	rst				: out std_logic;
- 	pb_n_filtered	: in  std_logic_vector (3 downto 0);
-	pb					: out	std_logic_vector(3 downto 0)							 
+	rst_n				: in	std_logic; -- reset (active low) is passed in here as it needs the same logic applied as our physicals PB's
+	rst				: out std_logic; -- reset (active high) output after logic applied
+ 	pb_n_filtered	: in  std_logic_vector (3 downto 0); -- all 4 push buttons passed in as they are active low
+	pb					: out	std_logic_vector(3 downto 0) -- outputted push buttons after logic are active high
 	); 
 end PB_inverters;
 
+-- architecture definition
 architecture ckt of PB_inverters is
 
 begin
+-- negating the active low reset to make it active high
 rst <= NOT(rst_n);
+-- negating the active low pb vector to make it active high
 pb <= NOT(pb_n_filtered);
 
 
